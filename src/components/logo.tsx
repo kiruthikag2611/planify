b07b5a3dd2085122b66d7974987193f31400323b
DHANYAA M R <dhanyaamr.it25@bitsathy.ac.in>
2025-11-20 06:27:11 +0000

    I CAN'T SEE MY IMAGE

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,17 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { PlaceHolderImages } from "@/lib/placeholder-images";
 
 export function PlanifyLogo({ className }: { className?: string }) {
+  const logoImage = PlaceHolderImages.find(p => p.id === 'planify-logo');
+  
+  if (!logoImage) {
+    return <div className={cn("w-24 h-24 bg-red-500", className)}>Logo not found</div>;
+  }
+
   return (
     <Image
-      src="/planify-logo.png"
+      src={logoImage.imageUrl}
       alt="Planify Logo"
       width={300}
       height={300}
