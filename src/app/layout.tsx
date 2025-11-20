0440292623b29accf86728ab3a65ddb124ed0a3d
DHANYAA M R <dhanyaamr.it25@bitsathy.ac.in>
2025-11-20 10:41:05 +0000

    Create a soft, cozy, aesthetic background illustration for a productivit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const backgroundImage = PlaceHolderImages.find(img => img.id === 'illustrated-desk-background');
+  const backgroundImage = PlaceHolderImages.find(img => img.id === 'cozy-study-desk-illustration');
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -36,7 +36,7 @@ export default function RootLayout({
             backgroundAttachment: 'fixed',
         }}
       >
-        <div className="absolute inset-0 bg-black/20 z-0"></div>
+        <div className="absolute inset-0 bg-black/30 z-0"></div>
         <Providers>
           <FirebaseErrorListener />
           <SidebarProvider>
