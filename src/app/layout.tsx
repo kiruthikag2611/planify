a07af3c886720f64dac65e0272af8be5db615883
DHANYAA M R <dhanyaamr.it25@bitsathy.ac.in>
2025-11-21 03:20:48 +0000

    undo the changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const backgroundImage = PlaceHolderImages.find(img => img.id === 'welcome-background');
+  const backgroundImage = PlaceHolderImages.find(img => img.id === 'cozy-study-ambience');
 
   return (
     <html lang="en" suppressHydrationWarning>
