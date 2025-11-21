6f4abca2425dbb1398ac25286d8e5720e1509637
DHANYAA M R <dhanyaamr.it25@bitsathy.ac.in>
2025-11-21 09:59:21 +0000

    the movement is too much ...make it suttle

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -100,7 +100,7 @@ export default {
         },
         'float': {
           '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)' },
+          '50%': { transform: 'translateY(-4px)' },
         },
       },
       animation: {
